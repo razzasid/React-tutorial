7ac0aee54e2d554d6c64a98255f6f1c9feda2bc0
Mohd Raza <razamohd6395@gmail.com>
2025-02-25 22:34:12 +0530

    parcel intro

---
+++ 01parcelreact/script.js
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+const h1 = <h1>Hello Parcel</h1>;
+
+const root = createRoot(document.getElementById("root"));
+root.render(h1);
+
+// console.log(React);
+console.log("Hello world");
