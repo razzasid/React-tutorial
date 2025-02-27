24a0aaa7356768a274fc24f5bba37fa90d28d8f6
Mohd Raza <razamohd6395@gmail.com>
2025-02-27 22:34:21 +0530

    conditional Rendering

--- 04appleBasket/Components/Basket.js
+++ 04appleBasket/Components/Basket.js
@@ -1,12 +1,14 @@
-import React from "react";
-
 const Basket = ({ appleCount, basketName }) => {
   return (
     <div className="basket">
       <h1>
         <span>{appleCount}</span> Apples
       </h1>
-      <p>{basketName}</p>
+      <p>
+        {basketName} {appleCount === 10 && "(Full)"}{" "}
+        {appleCount === 0 && "(Empty)"}
+        {appleCount >= 5 && appleCount != 10 && "(Half Full)"}
+      </p>
     </div>
   );
 };
