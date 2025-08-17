ee060e10cba4313880e6e2efa89bb44ad6b35ab1
Raza Siddique <razamohd6395@gmail.com>
2025-08-17 12:31:37 +0530

    Learn useRef

--- 08 React Forms/By React/components/ExpenseForm.jsx
+++ 08 React Forms/By React/components/ExpenseForm.jsx
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function ExpenseForm({ setExpenses }) {
   //   const [title, setTitle] = useState("");
   //   const [category, setCategory] = useState("");
   //   const [amount, setAmount] = useState("");
 
+  {
+    /* 
+      - On every state varibale change our component re-renders
+      - Between the re-renders if we want to persist its value we used useRef although usestate varible also persists or maintain its value but whenever state varibale change its re-render the page but on useRef does'nt trigger re-render on the other hand a normal variable like this (let myNum = 0) it neither re-render the page and not persist its value on every re-render that the basic difference between in all these three types of variables;
+      */
+  }
+
+  // - Our component does'nt re-render on both this varibale change
+  // const myRef = useRef(0);
+  // let myNum = 0;
+
+  /* - The most common use of useRef is to ref prop on input so it gives us a DOM access of that particular nore where we put the Ref prop and then we do anything we do in vanilla js like changing color like this myRef.current.style.backgroundColor = "red"; or accessing input value through its value attribut like this myRef.current.value  */
+
+  // const titleRef = useRef(null);
+  // const categoryRef = useRef(null);
+  // const amountRef = useRef(null);
+
+  // useEffect(() => {
+  //   console.log();
+  // });
+
+  //Component re-render everytime we change this state variable
   const [expense, setExpense] = useState({
     title: "",
     category: "",
@@ -14,16 +36,32 @@ function ExpenseForm({ setExpenses }) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    //State-method
+
     setExpenses((prevState) => [
       ...prevState,
       { ...expense, id: crypto.randomUUID() },
     ]);
 
-    setExpense({
-      title: "",
-      category: "",
-      amount: 0,
-    });
+    //UseRef - method
+
+    // setExpenses((prevState) => [
+    //   ...prevState,
+    //   {
+    //     title: titleRef.current.value,
+    //     category: categoryRef.current.value,
+    //     amount: amountRef.current.value,
+    //     id: crypto.randomUUID(),
+    //   },
+    // ]);
+
+    //Form Method
+
+    // setExpense({
+    //   title: "",
+    //   category: "",
+    //   amount: 0,
+    // });
     // const expense = { ...getFormData(e.target), id: crypto.randomUUID() };
     // setExpenses((prevExpenses) => [...prevExpenses, expense]);
     // e.target.reset();
@@ -43,17 +81,22 @@ function ExpenseForm({ setExpenses }) {
       <div className="input-container">
         <label htmlFor="title">Title</label>
         <input
+          // ref={titleRef}
           onChange={(e) =>
-            setExpense((prevState) => ({ ...prevState, title: e.target.value }))
+            setExpense((prevState) => ({
+              ...prevState,
+              title: e.target.value,
+            }))
           }
           value={expense.title}
-          //   name="title"
+          name="title"
           id="title"
         />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
         <select
+          // ref={categoryRef}
           onChange={(e) =>
             setExpense((prevState) => ({
               ...prevState,
@@ -61,7 +104,7 @@ function ExpenseForm({ setExpenses }) {
             }))
           }
           value={expense.category}
-          //   name="category"
+          name="category"
           id="category"
         >
           <option hidden>Select Category</option>
@@ -75,6 +118,7 @@ function ExpenseForm({ setExpenses }) {
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
         <input
+          // ref={amountRef}
           onChange={(e) =>
             setExpense((prevState) => ({
               ...prevState,
@@ -82,7 +126,7 @@ function ExpenseForm({ setExpenses }) {
             }))
           }
           value={expense.amount}
-          //   name="amount"
+          name="amount"
           id="amount"
         />
       </div>
